9a49c23389abfbbbe23b7712c7eb6bf3b173fe35
Robin Thomas <robinthomas@Robins-MacBook-Pro.local>
2025-10-17 09:55:00 +0530

    update base url

--- src/utils/api.util.ts
+++ src/utils/api.util.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: import.meta.env.VITE_PUBLIC_API_URL,
+  // baseURL: import.meta.env.VITE_PUBLIC_API_URL,
+  baseURL: "https://api.fairsplits.in:9090",
 });
 
 export default apiInstance;
